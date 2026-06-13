ad1329861862c9f1f019a0f24d14cada31286c9e
Sima-akhter <simaakter5301@gmail.com>
2026-06-14 00:01:16 +0600

    update action file

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,3 +1,10 @@
-const deleteUser = async (id) => { 
-    
+const deleteUser = async (usersId) => { 
+'use server';
+    const res = await fetch(`http://localhost:5000/users/${usersId}`, {
+        method: "DELETE"
+    });
+    const data = await res.json();
+    return data;
+
+
 }
